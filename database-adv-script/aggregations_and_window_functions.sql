eb1e1271e61f75ba6362769884df491b067e9ed3
UGBERAESE CHARLES A <ugberaeseac@gmail.com>
2025-05-18 17:59:33 -0400

    SQL aggregation and window function

--- database-adv-script/aggregations_and_window_functions.sql
+++ database-adv-script/aggregations_and_window_functions.sql
@@ -5,6 +5,6 @@ GROUP BY user_id;
 
 SELECT property_id,
 COUNT(*) AS total_bookings,
-RANK() OVER (ORDER BY COUNT(*) DESC) AS property_rank
+ROW_NUMBER() OVER (ORDER BY COUNT(*) DESC) AS property_rank
 FROM bookings
 GROUP BY property_id;
