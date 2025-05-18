2812ecfd64c46e18366de899cf9fc8c18d345a76
UGBERAESE CHARLES A <ugberaeseac@gmail.com>
2025-05-18 16:57:19 -0400

    correlated and non-correlated subqueries

--- database-adv-script/subqueries.sql
+++ database-adv-script/subqueries.sql
@@ -4,7 +4,7 @@ INNER JOIN (select reviews.property_id, round(AVG(rating)) as avg_rating
 	FROM reviews
 	GROUP BY reviews.property_id) avg_rating_property
 ON properties.property_id = avg_rating_property.property_id
-WHERE avg_rating_property.avg_rating > 4;
+WHERE avg_rating_property.avg_rating > 4.0;
 
 SELECT first_name, last_name
 FROM users
