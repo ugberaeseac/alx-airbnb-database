61d61e33aecb9925f836661d447641b4b3d80e42
UGBERAESE CHARLES A <ugberaeseac@gmail.com>
2025-05-18 13:44:06 -0400

    fix SQL query

--- database-adv-script/joins_queries.sql
+++ database-adv-script/joins_queries.sql
@@ -1,14 +1,17 @@
 SELECT *
 FROM bookings
 INNER JOIN users
-ON bookings.user_id = users.user_id;
+ON bookings.user_id = users.user_id
+ORDER BY bookings.user_id;
 
 SELECT *
 FROM properties
 LEFT JOIN reviews
-ON properties.property_id = reviews.property_id;
+ON properties.property_id = reviews.property_id
+ORDER BY properties.property_id;
 
 SELECT *
 FROM users
-FULL JOIN bookings
-ON users.user_id = bookings.user_id;
+FULL OUTER JOIN bookings
+ON users.user_id = bookings.user_id
+ORDER BY users.user_id;
