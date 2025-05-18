76ca15c680666c7738da7cf0fbc5d7fa06d16300
UGBERAESE CHARLES A <ugberaeseac@gmail.com>
2025-05-18 16:45:30 -0400

    correlated and non-correlated subqueries

---
+++ database-adv-script/subqueries.sql
@@ -0,0 +1,14 @@
+SELECT properties.property_id, name, description, avg_rating
+FROM properties
+INNER JOIN (select reviews.property_id, round(AVG(rating)) as avg_rating
+	FROM reviews
+	GROUP BY reviews.property_id) avg_rating_property
+ON properties.property_id = avg_rating_property.property_id
+WHERE avg_rating_property.avg_rating > 4;
+
+SELECT first_name, last_name
+FROM users
+WHERE (
+	SELECT COUNT(*) 
+	FROM bookings
+	WHERE bookings.user_id = users.user_id) > 3 ;
